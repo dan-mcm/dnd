44dff3d2e62511d1af08747223a2d13376d1e563
daniel40392 <daniel40392@gmail.com>
2019-11-04 16:52:29 +0000

    Updates

---
+++ precache-manifest.43458e51fe0c73ea2dfe8fa13e83ca69.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc531016a0f11f081e09b7baa032c116",
+    "url": "/dnd/index.html"
+  },
+  {
+    "revision": "ec0e105a30c1a72a84d9",
+    "url": "/dnd/static/css/main.318a8c91.chunk.css"
+  },
+  {
+    "revision": "438130d0ed2c5d7b8cb8",
+    "url": "/dnd/static/js/2.e2cae9aa.chunk.js"
+  },
+  {
+    "revision": "ec0e105a30c1a72a84d9",
+    "url": "/dnd/static/js/main.638ab04c.chunk.js"
+  },
+  {
+    "revision": "b87e66e1d41392c70798",
+    "url": "/dnd/static/js/runtime-main.d2ca28ae.js"
+  },
+  {
+    "revision": "feb512e42ecb420f612e365132b1cfbf",
+    "url": "/dnd/static/media/D10.feb512e4.png"
+  },
+  {
+    "revision": "92dc28f36612b43dc77f8dbb2cc5cf9e",
+    "url": "/dnd/static/media/D100.92dc28f3.png"
+  },
+  {
+    "revision": "e553ce6bf20019f7e50552b9ea86be2c",
+    "url": "/dnd/static/media/D12.e553ce6b.png"
+  },
+  {
+    "revision": "45966914e29a80a2b70a6c423a0728b0",
+    "url": "/dnd/static/media/D20.45966914.png"
+  },
+  {
+    "revision": "07d94c0b61c29c73ae119541a33f2e4e",
+    "url": "/dnd/static/media/D4.07d94c0b.png"
+  },
+  {
+    "revision": "db632cfc2df9a251bf7e1f1311ac494e",
+    "url": "/dnd/static/media/D6.db632cfc.png"
+  },
+  {
+    "revision": "d064020ab591187c16126bfff72baa8d",
+    "url": "/dnd/static/media/D8.d064020a.png"
+  },
+  {
+    "revision": "e3965f1b63c4cdd7e82e4f280327100c",
+    "url": "/dnd/static/media/dnd.e3965f1b.png"
+  },
+  {
+    "revision": "b1b64ac6b1bd41f228fee7be91eae791",
+    "url": "/dnd/static/media/dragon.b1b64ac6.jpg"
+  },
+  {
+    "revision": "633f230e8fb062adc66fb11f9205b73a",
+    "url": "/dnd/static/media/stained.633f230e.jpg"
+  }
+]);
